34d2c69f79f8063aaaf3ba6e110873a6947be834
Md. Ikramul Hassan <123355919+Ikramul-Sakib@users.noreply.github.com>
2024-11-17 02:20:44 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -12,40 +12,41 @@ struct used {
 int user_type(void);
 int security(void);
 bool val_phn(const char* phone);
+bool val_phn2(char phone[]);
 bool val_date(const char* date);
 void log_in(char num[100], int p);
-void log_transaction(const char* phone, const char* action, int amount);
-void view_transactions(const char* phone);
+void load_trans(const char* phone, const char* action, int amount);
+void view_trans(const char* phone);
 void change_pin(const char* phone, int* current_pin);
 
-void log_transaction(const char* phone, const char* action, int amount) {
-    FILE* transFile = fopen("/Users/mdikramulhassan/Desktop/Swift_Project/proj/proj/transactions.txt", "a");
-    if (transFile != NULL) {
-        fprintf(transFile, "%s Action: %s, Amount: %d\n",phone,action, amount);
-        fclose(transFile);
+void load_trans(const char* phone, const char* action, int amount) {
+    FILE* file = fopen("/Users/mdikramulhassan/Desktop/Swift_Project/proj/proj/transactions.txt", "a");
+    if (file != NULL) {
+        fprintf(file, "%s Action: %s, Amount: %d\n",phone,action, amount);
+        fclose(file);
     } else {
         printf("Error: Unable to log the transaction.\n");
     }
 }
 
-void view_transactions(const char* phone) {
-    FILE* transFile = fopen("/Users/mdikramulhassan/Desktop/Swift_Project/proj/proj/transactions.txt", "r");
+void view_trans(const char* phone) {
+    FILE* file = fopen("/Users/mdikramulhassan/Desktop/Swift_Project/proj/proj/transactions.txt", "r");
     char line[200];
     bool found = false;
 
-    if (transFile == NULL) {
+    if (file == NULL) {
         printf("No transaction history found.\n");
         return;
     }
 
     printf("\nTransaction history for phone:\n");
-    while (fgets(line, sizeof(line), transFile)) {
+    while (fgets(line, sizeof(line), file)) {
         if (strstr(line, phone) != NULL) {
             printf("%s", line);
             found = true;
         }
     }
-    fclose(transFile);
+    fclose(file);
 
     if (!found) {
         printf("No transactions found for this phone.\n");
@@ -77,14 +78,14 @@ void change_pin(const char* phone, int* current_pin) {
     FILE* tempFile = fopen(tempFileName, "w");
 
     char line[200];
-    char name[100], dob[11], phone_in_file[14];
+    char name[100], dob[11], phn[14];
     int pin_in_file, nid;
 
     while (fgets(line, sizeof(line), file)) {
-        sscanf(line, "%[^,],%[^,],%[^,],%d,%d", name, dob, phone_in_file, &pin_in_file, &nid);
-        if (strcmp(phone_in_file, phone) == 0) {
+        sscanf(line, "%[^,],%[^,],%[^,],%d,%d", name, dob, phn, &pin_in_file, &nid);
+        if (strcmp(phn, phone) == 0) {
            
-            fprintf(tempFile, "%s,%s,%s,%d,%d\n", name, dob, phone_in_file, new_pin, nid);
+            fprintf(tempFile, "%s,%s,%s,%d,%d\n", name, dob, phn, new_pin, nid);
             *current_pin = new_pin;
         } else {
             fprintf(tempFile, "%s", line);
@@ -128,13 +129,13 @@ bool val_date(const char* date) {
         return false;
     }
     
-    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int days_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-        days_in_month[1] = 29;
+        days_month[1] = 29;
     }
     
-    if (day < 1 || day > days_in_month[month - 1]) {
+    if (day < 1 || day > days_month[month - 1]) {
         return false;
     }
     
@@ -150,6 +151,22 @@ bool val_phn(const char* phone) {
         return false;
     }
     for (int i = 5; i < 14; i++) {
+        if (phone[i] < '0' || phone[i] > '9') {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool val_phn2( char phone[]) {
+ 
+    if (strlen(phone) != 11) {
+        return false;
+    }
+    if (strncmp(phone, "01", 2) != 0) {
+        return false;
+    }
+    for (int i = 2; i < 11; i++) {
         if (phone[i] < '0' || phone[i] > '9') {
             return false;
         }
@@ -232,30 +249,30 @@ int user_type(void){
 }
 
 void log_in(char num[100], int p) {
-    FILE *filePointer;
-    char dataToBeRead[200];
-    bool login_success = false;
+    FILE *fptr;
+    char dataread[200];
+    bool success = false;
 
-    while (!login_success) {
-        filePointer = fopen("/Users/mdikramulhassan/Desktop/Swift_Project/proj/proj/file.txt", "r");
+    while (!success) {
+        fptr = fopen("/Users/mdikramulhassan/Desktop/Swift_Project/proj/proj/file.txt", "r");
 
-        if (filePointer == NULL) {
+        if (fptr == NULL) {
             printf("Error: Unable to open the file.\n");
             return;
         }
 
-        while (fgets(dataToBeRead, sizeof(dataToBeRead), filePointer) != NULL) {
+        while (fgets(dataread, sizeof(dataread), fptr) != NULL) {
         
             char file_name[100], file_dob[11], file_phone[14];
             int file_pin, file_nid;
 
       
-            if (sscanf(dataToBeRead, "%[^,],%[^,],%[^,],%d,%d",
+            if (sscanf(dataread, "%[^,],%[^,],%[^,],%d,%d",
                        file_name, file_dob, file_phone, &file_pin, &file_nid) == 5) {
               
                 if (strcmp(file_phone, num) == 0 && file_pin == p) {
                     printf("Login successful! Welcome, %s.\n", file_name);
-                    login_success = true;
+                    success = true;
                     
                     //strcpy(logged_in_phone, file_phone);
                     break;
@@ -263,9 +280,9 @@ void log_in(char num[100], int p) {
             }
         }
 
-        fclose(filePointer);
+        fclose(fptr);
 
-        if (!login_success) {
+        if (!success) {
             printf("Login failed: Invalid phone number or PIN.\n");
             printf("Enter your Phone Number: ");
             scanf("%s", num);
@@ -281,8 +298,8 @@ void log_in(char num[100], int p) {
 int main(void) {
     int pn = user_type();
 
-    int c,p,num,amount,balance = 100000,out;
-    
+    int c,p,amount,balance = 100000,out;
+    char num[11];
     while (1)
     {
         printf("What type of operatiobn you want to do with: \n1. Send Money\n2. Send Money to non user\n3. Mobile Recharge\n4. Payment\n5. Cash Out\n6. Add Money\n7. Balance. \n8. Transcaction history. \n9. Change pin. \n10. Exit. \n");
@@ -290,8 +307,14 @@ int main(void) {
         scanf("%d",&c);
         
         if (c==1) {
-            printf("Enter wallet number which you want to send money : ");
-            scanf("%d",&num);
+            do {
+                        printf("Enter wallet number which you want to send money(Start with 01): ");
+                        scanf("%s", num);
+                        if (!val_phn2(num)) {
+                            printf("Invalid phone number. Please enter exactly 11 digits.\n");
+                        }
+                    }
+            while (!val_phn2(num));
             printf("Enter the amount: ");
             scanf("%d",&amount);
         
@@ -300,7 +323,7 @@ int main(void) {
             if (p == pn) {
                 printf("Success");
                 balance = balance-amount;
-                log_transaction(logged_in_phone, "Send Money", amount);
+                load_trans(logged_in_phone, "Send Money", amount);
             }
             else{
                 printf("Failed");
@@ -310,8 +333,15 @@ int main(void) {
         }
         
         else if (c==2){
-            printf("Enter non wallet number which you want to send money : ");
-            scanf("%d",&num);
+            do {
+                        printf("Enter non wallet number which you want to send money (Start with 01): ");
+                        scanf("%s", num);
+                        if (!val_phn2(num)) {
+                            printf("Invalid phone number. Please enter exactly 11 digits.\n");
+                        }
+                    }
+            while (!val_phn2(num));
+            
             printf("Enter the amount: ");
             scanf("%d",&amount);
             printf("Enter your pin (only number): ");
@@ -320,7 +350,7 @@ int main(void) {
             if (p == pn) {
                 printf("Success");
                 balance = balance-amount;
-                log_transaction(logged_in_phone, "Send Money to non user", amount);
+                load_trans(logged_in_phone, "Send Money to non user", amount);
             }
             else{
                 printf("Failed");
@@ -329,16 +359,22 @@ int main(void) {
                         
         }
         else if (c == 3){
-            printf("Enter your mobile number to recharge: ");
-            scanf("%d",&num);
+            do {
+                        printf("Enter your mobile number to recharge(Start with 01): ");
+                        scanf("%s", num);
+                        if (!val_phn2(num)) {
+                            printf("Invalid phone number. Please enter exactly 11 digits.\n");
+                        }
+                    }
+            while (!val_phn2(num));
             printf("Entr recharge amount: ");
             scanf("%d",&amount);
             printf("Enter your pin (only number): ");
             scanf("%d",&p);
             if (p == pn) {
-                printf("%d bdt was successfully recharge to %d.",num,amount);
+                printf("%d bdt was successfully recharge to %s.",amount,num);
                 balance = balance - amount;
-                log_transaction(logged_in_phone, "Mobile Recharge", amount);
+                load_trans(logged_in_phone, "Mobile Recharge", amount);
             }
             else{
                 printf("Pin number is not correct.");
@@ -347,23 +383,28 @@ int main(void) {
         }
         
         else if (c == 4){   //payment
-            printf("Enter the marchent number to continue: ");
-            scanf("%d",&num);
+            do {
+                        printf("Enter your marchent number (Start with 01): ");
+                        scanf("%s", num);
+                        if (!val_phn2(num)) {
+                            printf("Invalid phone number. Please enter exactly 11 digits.\n");
+                        }
+                    }
+            while (!val_phn2(num));
             printf("Entr amount: ");
             scanf("%d",&amount);
             printf("Enter your pin (only number): ");
             scanf("%d",&p);
             if (p == pn) {
-                printf("%d bdt was successfully recharge to %d.",num,amount);
+                printf("%d bdt was successfully paid to %s.",amount,num);
                 balance = balance - amount;
-                log_transaction(logged_in_phone, "Payment", amount);
+                load_trans(logged_in_phone, "Payment", amount);
             }
             else{
                 printf("Pin number is not correct.");
             }
             printf("\n");
         }
-        
         
         else if (c == 5){   //cash out
             printf("By which service you want to cash out:\n1. ATM Bank.\n2. Agent.\n");
@@ -378,22 +419,29 @@ int main(void) {
                     printf("Your generated code is : %ld\n",ra);
                     printf("Enter the above code to ATM machine.");
                     balance = balance - amount;
-                    log_transaction(logged_in_phone, "Cash Out", amount);
+                    load_trans(logged_in_phone, "Cash Out", amount);
                 }
                 else{
                     printf("Wrong Pin code.");
                 }}
             else if (out == 2){
-                printf("Enter the agent number to cash out: ");
-                scanf("%d",&num);
+                do {
+                            printf("Enter the agent number to cash out(Start with 01): ");
+                            scanf("%s", num);
+                            if (!val_phn2(num)) {
+                                printf("Invalid phone number. Please enter exactly 11 digits.\n");
+                            }
+                        }
+                while (!val_phn2(num));
+                
                 printf("Entr your amount: ");
                 scanf("%d",&amount);
                 printf("Enter your pin (only number): ");
                 scanf("%d",&p);
                 if (p == pn) {
-                    printf("Cash out to number: %d and amount: %d was successful.",num,amount);
+                    printf("Cash out to number: %s and amount: %d was successful.",num,amount);
                     balance = balance - amount;
-                    log_transaction(logged_in_phone, "Cash Out", amount);
+                    load_trans(logged_in_phone, "Cash Out", amount);
                 }
                 else{
                     printf("Wrong Pin code.");
@@ -403,7 +451,7 @@ int main(void) {
         }
         else if(c == 6){
             int ca;
-            printf("Add Money:\n1. From Bank\n2. Agent");
+            printf("Add Money:\n1. From Bank\n2. Agent\n");
             scanf("%d",&ca);
             if (ca == 1) {
                 int bank;
@@ -412,27 +460,28 @@ int main(void) {
                 printf("Entr your amount: ");
                 scanf("%d",&amount);
                 balance = balance + amount;
-                log_transaction(logged_in_phone, "Add Money", amount);
+                load_trans(logged_in_phone, "Add Money", amount);
             }
             else if (ca ==2){
                 printf("Please Provide Your number to Agent.");
                 printf("Entr your amount: ");
                 scanf("%d",&amount);
                 balance = balance + amount;
-                log_transaction(logged_in_phone, "Add Money", amount);
+                load_trans(logged_in_phone, "Add Money", amount);
             }
         }
         else if (c==7){
             printf("Your current balance is: %d",balance);
         }
         else if (c == 8){//transaction history
-            view_transactions(logged_in_phone);
+            view_trans(logged_in_phone);
         }
         else if (c == 9){// pin change
             change_pin(logged_in_phone, &pn);
         }
-        else if(c == 10){   
+        else if(c == 10){
             return 0;
+            break;
         }
         printf("\n");
         
